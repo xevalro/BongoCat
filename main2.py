3802dae03e18d8474dfb6c74eaebbc85be3198f2
EvanRothenbacher10 <825585@mystma.org>
2022-09-13 09:39:36 -0500

    Mr Osowski 3 Functions

---
+++ main2.py
@@ -0,0 +1,29 @@
+from threading import *
+import threading
+
+
+firstName = "Evan "
+lastName = "Rothenbacher"
+grade = 10
+age = 15
+garfield = "Garfield is amazing"
+
+
+def printName():
+    print(firstName + lastName)
+
+
+def addGradeAge():
+    print(grade + age)
+
+def printGarfield():
+    while True:
+        print(garfield)
+
+
+addGradeAge()
+printName()
+
+garfieldThread = Thread(target=printGarfield)
+
+garfieldThread.start()
